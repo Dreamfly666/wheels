27e5c913c6044f8896730f4df0741ff3c7ea7f52
lishanpeng <18562011169@163.com>
2018-12-06 13:47:14 +0800

    create tabs

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+gulp.task('browser-sync',function(){
+    browserSync.init({
+        files:['./lib/**/*.js','./demos/*.html'],
+        server:{
+            baseDir:'./',
+            index:'demos/tabs.html'
+        },
+        port:8010
+    })
+})
+gulp.task('default',['browser-sync'])
